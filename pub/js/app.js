ef2c504598802706a3a862a699c680cf3f32edd8
Peng Zhong <peng@nylira.com>
2015-01-28 00:07:08 +0800

    dropping blocks now work

--- pub/js/app.js
+++ pub/js/app.js
@@ -1,8 +1,8 @@
 'use strict'
 
-/*----------------------------------------------------------------------------*/
-//  Modules
-/*----------------------------------------------------------------------------*/
+/*============================================================================*/
+// Modules
+/*============================================================================*/
 
 var P = require('pixi.js')
 var _ = require('lodash')
@@ -12,18 +12,24 @@ var _ = require('lodash')
 //var attachFastClick = require('fastclick')
 //attachFastClick(document.body)
 
-/*----------------------------------------------------------------------------*/
-//  Variables
-/*----------------------------------------------------------------------------*/
+/*============================================================================*/
+// Constants
+/*============================================================================*/
 
 // constants
 var R = window.devicePixelRatio
 var CANVAS_X = 320*R
 var CANVAS_Y = 568*R
 var GRID_UNIT = 16*R
+var REFRESH_RATE = 50
+
+/*============================================================================*/
+// Variables
+/*============================================================================*/
 
 // stage variables
 var stage, renderer
+var timer = new Date().getTime() + REFRESH_RATE
 
 // scenes
 var sceneMenu
@@ -31,38 +37,59 @@ var sceneGame
 var sceneSummary
 
 // textures
-var TDemo
+var TBlockRed
+var TBackground
 
 // sprites
-var SDemo
+var SActivePiece
+var SBackground
 
-/*----------------------------------------------------------------------------*/
-//  Stage
-/*----------------------------------------------------------------------------*/
+/*============================================================================*/
+// Stage
+/*============================================================================*/
 
 stage = new P.Stage(0xFFFFFF)
 renderer = P.autoDetectRenderer(CANVAS_X, CANVAS_Y)
 document.getElementById('container').appendChild(renderer.view)
 
-/*----------------------------------------------------------------------------*/
+/*============================================================================*/
 //  Textures
-/*----------------------------------------------------------------------------*/
+/*============================================================================*/
 
 if(R === 2){
-  TDemo = new P.Texture.fromImage('../img/block16x16red@x2.png')
+  TBlockRed = new P.Texture.fromImage('../img/block16x16red@x2.png')
+  TBackground = new P.Texture.fromImage('../img/darkGrid16x16@x2.png')
 } else {
-  TDemo = new P.Texture.fromImage('../img/block16x16red.png')
+  TBlockRed = new P.Texture.fromImage('../img/block16x16red.png')
+  TBackground = new P.Texture.fromImage('../img/darkGrid16x16.png')
 }
 
-/*----------------------------------------------------------------------------*/
-//  Sprites
-/*----------------------------------------------------------------------------*/
+/*============================================================================*/
+// Sprites
+/*============================================================================*/
 
-SDemo = new P.Sprite(TDemo)
-SDemo.position.x = 100*R
-SDemo.position.y = 100*R
-stage.addChild(SDemo)
+var fieldOfPlay = new P.DisplayObjectContainer()
+var occupied = []
 
+var gridHeight = 24
+var gridWidth = 12
+function makeMap(width, height) {
+  for(var i=0; i < height; i++) {
+    for(var j=0; j < width; j++) {
+      var sprite = new P.Sprite(TBackground)
+      sprite.position.x =  GRID_UNIT * j
+      sprite.position.y = GRID_UNIT * i
+      fieldOfPlay.addChild(sprite)
+    }
+  }     
+}
+makeMap(gridWidth, gridHeight)
+
+stage.addChild(fieldOfPlay)
+
+createNewPiece()
+  
+console.log(_.find([[1,3],[1,2],[3,4]], [1,2]))
 /*----------------------------------------------------------------------------*/
 //  Update
 /*----------------------------------------------------------------------------*/
@@ -70,5 +97,47 @@ stage.addChild(SDemo)
 requestAnimationFrame(update)
 function update() {
   requestAnimationFrame(update)
+
+  if(timer < new Date().getTime()) {
+
+    // if the moving piece hits another piece, keep it there
+    if( occupied.length > 0 && _.find(occupied, [ SActivePiece.position.x
+    , SActivePiece.position.y + GRID_UNIT]) !== undefined) {
+
+      occupied.push([SActivePiece.position.x, SActivePiece.position.y])
+      console.log("this should rest on top of the other")
+
+      createNewPiece()
+
+    // if the moving piece hits the floor, keep it there
+    } else if(SActivePiece.position.y === gridHeight * GRID_UNIT - GRID_UNIT) {
+
+      // add old moving piece to occupied list
+      occupied.push([SActivePiece.position.x, SActivePiece.position.y])
+      //console.log(occupied)
+
+      createNewPiece()
+
+    // otherwise keep it moving
+    } else {
+
+      SActivePiece.position.y = SActivePiece.position.y + GRID_UNIT
+      timer = new Date().getTime() + REFRESH_RATE
+
+    }
+  }
+
   renderer.render(stage)
+
+}
+
+/*----------------------------------------------------------------------------*/
+// Helpers
+/*----------------------------------------------------------------------------*/
+
+function createNewPiece() {
+  SActivePiece = new P.Sprite(TBlockRed)
+  SActivePiece.position.x = _.random(0, gridWidth - 1) * GRID_UNIT
+  SActivePiece.position.y = 0
+  fieldOfPlay.addChild(SActivePiece)
 }
