b069797be078968e1b39a1aaceeccb16e41e6de9
Peng Zhong <peng@nylira.com>
2015-02-02 19:09:15 +0800

    cleaned up some rotation code in preparation for rotating other blocks

--- pub/js/rotateFP.js
+++ pub/js/rotateFP.js
@@ -1,6 +1,5 @@
 function rotateFP(fp, fpType, fpTypeState, occupied, GRID_WIDTH, GU) {
   var newOrientation
-  console.log('fpTypeState', FourPieceTypeState)
   switch(fpType) {
     case 'I':
       newOrientation = rotateI(fp, fpTypeState, occupied, GRID_WIDTH, GU)
@@ -37,83 +36,67 @@ function blocked(fp, occupied, GRID_WIDTH, GU) {
   return val
 }
 
-function setPositions(fp, futurePositions) {
-  fp[0].position.x = futurePositions[0][0]
-  fp[0].position.y = futurePositions[0][1]
-  fp[1].position.x = futurePositions[1][0]
-  fp[1].position.y = futurePositions[1][1]
-  fp[2].position.x = futurePositions[2][0]
-  fp[2].position.y = futurePositions[2][1]
-  fp[3].position.x = futurePositions[3][0]
-  fp[3].position.y = futurePositions[3][1]
+function setPositions(fp, newPositions) {
+  fp[0].position.x = newPositions[0][0]
+  fp[0].position.y = newPositions[0][1]
+  fp[1].position.x = newPositions[1][0]
+  fp[1].position.y = newPositions[1][1]
+  fp[2].position.x = newPositions[2][0]
+  fp[2].position.y = newPositions[2][1]
+  fp[3].position.x = newPositions[3][0]
+  fp[3].position.y = newPositions[3][1]
   return fp
 }
 
 function rotateI(fp, fpTypeState, occupied, GRID_WIDTH, GU) {
-  var futurePositions = [[],[],[],[]]
+  var newPositions = [[],[],[],[]]
   var stop = false
   switch(fpTypeState) {
     case 1:
-      futurePositions[0][0] = fp[0].position.x + GU*2
-      futurePositions[0][1] = fp[0].position.y - GU
-      futurePositions[1][0] = fp[1].position.x + GU
-      futurePositions[1][1] = fp[1].position.y
-      futurePositions[2][0] = fp[2].position.x
-      futurePositions[2][1] = fp[2].position.y + GU
-      futurePositions[3][0] = fp[3].position.x - GU
-      futurePositions[3][1] = fp[3].position.y + GU*2
-      stop = blocked(futurePositions, occupied, GRID_WIDTH, GU)
+      newPositions[0] = [fp[0].position.x + GU*2, fp[0].position.y - GU  ]
+      newPositions[1] = [fp[1].position.x + GU  , fp[1].position.y       ]
+      newPositions[2] = [fp[2].position.x       , fp[2].position.y + GU  ]
+      newPositions[3] = [fp[3].position.x - GU  , fp[3].position.y + GU*2]
+      stop = blocked(newPositions, occupied, GRID_WIDTH, GU)
       if(stop === false) {
-        console.log('rotating 1')
-        setPositions(fp, futurePositions)
+        //console.log('rotating 1')
+        setPositions(fp, newPositions)
         fpTypeState = 2
       }
       break
     case 2:
-      futurePositions[0][0] = fp[0].position.x + GU
-      futurePositions[0][1] = fp[0].position.y + GU*2
-      futurePositions[1][0] = fp[1].position.x
-      futurePositions[1][1] = fp[1].position.y + GU
-      futurePositions[2][0] = fp[2].position.x - GU
-      futurePositions[2][1] = fp[2].position.y
-      futurePositions[3][0] = fp[3].position.x - GU*2
-      futurePositions[3][1] = fp[3].position.y - GU
-      stop = blocked(futurePositions, occupied, GRID_WIDTH, GU)
+      newPositions[0] = [fp[0].position.x + GU  , fp[0].position.y + GU*2]
+      newPositions[1] = [fp[1].position.x       , fp[1].position.y + GU  ]
+      newPositions[2] = [fp[2].position.x - GU  , fp[2].position.y       ]
+      newPositions[3] = [fp[3].position.x - GU*2, fp[3].position.y - GU  ]
+      stop = blocked(newPositions, occupied, GRID_WIDTH, GU)
       if(stop === false) {
-        console.log('rotating 2')
-        setPositions(fp, futurePositions)
+        //console.log('rotating 2')
+        setPositions(fp, newPositions)
         fpTypeState = 3
       }
       break
     case 3:
-      futurePositions[0][0] = fp[0].position.x - GU*2
-      futurePositions[0][1] = fp[0].position.y + GU
-      futurePositions[1][0] = fp[1].position.x - GU
-      futurePositions[1][1] = fp[1].position.y
-      futurePositions[2][0] = fp[2].position.x
-      futurePositions[2][1] = fp[2].position.y - GU
-      futurePositions[3][0] = fp[3].position.x + GU
-      futurePositions[3][1] = fp[3].position.y - GU*2
-      stop = blocked(futurePositions, occupied, GRID_WIDTH, GU)
+      newPositions[0] = [fp[0].position.x - GU*2, fp[0].position.y + GU  ]
+      newPositions[1] = [fp[1].position.x - GU  , fp[1].position.y       ]
+      newPositions[2] = [fp[2].position.x       , fp[2].position.y - GU  ]
+      newPositions[3] = [fp[3].position.x + GU  , fp[3].position.y - GU*2]
+      stop = blocked(newPositions, occupied, GRID_WIDTH, GU)
       if(stop === false) {
-        console.log('rotating 3')
-        setPositions(fp, futurePositions)
+        //console.log('rotating 3')
+        setPositions(fp, newPositions)
         fpTypeState = 4
       }
       break
     case 4:
-      futurePositions[0][0] = fp[0].position.x - GU
-      futurePositions[0][1] = fp[0].position.y - GU*2
-      futurePositions[1][0] = fp[1].position.x
-      futurePositions[1][1] = fp[1].position.y - GU
-      futurePositions[2][0] = fp[2].position.x + GU
-      futurePositions[2][1] = fp[2].position.y
-      futurePositions[3][0] = fp[3].position.x + GU*2
-      futurePositions[3][1] = fp[3].position.y + GU
-      stop = blocked(futurePositions, occupied, GRID_WIDTH, GU)
+      newPositions[0] = [fp[0].position.x - GU  , fp[0].position.y - GU*2]
+      newPositions[1] = [fp[1].position.x       , fp[1].position.y - GU  ]
+      newPositions[2] = [fp[2].position.x + GU  , fp[2].position.y       ]
+      newPositions[3] = [fp[3].position.x + GU*2, fp[3].position.y + GU  ]
+      stop = blocked(newPositions, occupied, GRID_WIDTH, GU)
       if(stop === false) {
-        console.log('rotating 4')
-        setPositions(fp, futurePositions)
+        //console.log('rotating 4')
+        setPositions(fp, newPositions)
         fpTypeState = 1
       }
       break
