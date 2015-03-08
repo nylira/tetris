a7d51fbaafc70f98ced27ad0efa96a94b04f623d
Peng Zhong <peng@nylira.com>
2015-03-08 18:01:23 +0800

    setup scene summary later

--- pub/js/app.js
+++ pub/js/app.js
@@ -319,7 +319,10 @@ function startNewGame(){
 
 function endGame(){
   STATE.gameRunning = false
+
+  setupSceneSummary()
   switchScenes(SCENES, SCENES.summary)
+
   TEXTS.summary.points.setText('Your Score: ' + STATE.score)
   updateHighScores(STATE.score, DB)
   lsdb.update(DB_NAME, DB)
@@ -500,7 +503,6 @@ function setupAll() {
   setupStage()
   setupSceneMenu()
   setupSceneGame()
-  setupSceneSummary()
 }
 
 /*----------------------------------------------------------------------------*/
